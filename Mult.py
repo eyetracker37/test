569c55e42a897ed8aa8db4aaa7aaf6d551803133
redsklton <32234189+redsklton@users.noreply.github.com>
2017-09-25 17:12:52 -0700

    Added Multiply

---
+++ Mult.py
@@ -0,0 +1,6 @@
+def multiply(x,y):
+    total = 0
+    while x > 0:
+        x -= 1
+        total += y
+    return total
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from Mult import multiply
+
 print "hi"
+
+value = multiply(3, 4)
+print value
 
 # this is a comment
